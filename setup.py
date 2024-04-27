916c54192ce1655e0d6cf95d37405868098ae8f1
ygag-ananthu <118436967+ygag-ananthu@users.noreply.github.com>
2024-04-27 21:25:13 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mygeometry',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='Ananthu',
+    packages=find_packages(),
+)
